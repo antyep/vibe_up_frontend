1a4a036010bf8c84fa12098c011c14c6bbf25c20
kulde <ku1de@icloud.com>
2024-03-25 22:48:03 +0100

    Footer change fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,7 @@ export const Footer = () => {
   return (
     <footer className="footer-wrapper">
       <div className="main-footer">
-        <div className="main-footer-left">
-          <a href="instagram.com">Instagram</a>
-        </div>
+        <div className="main-footer-left"></div>
         <div className="main-footer-right">
           <a href="google.com">Legal Notice</a>
           <a href="google.com">Privacy Policy</a>
