e97259078c2c92899926eb33a6692903e1197f7b
kulde <ku1de@icloud.com>
2024-03-25 22:46:37 +0100

    MainRouter change

--- src/pages/MainRouter.jsx
+++ src/pages/MainRouter.jsx
@@ -3,11 +3,8 @@ import { Register } from "./Register/Register";
 import { Home } from "./Home/Home";
 import { Login } from "./Login/Login";
 import { Profile } from "./Profile/Profile";
-import { Artists } from "./Artists/Artists";
 import { Admin } from "./Admin/Admin";
-import { Contact } from "./Contact/Contact";
-import { CreateAppointment } from "./CreateAppointment/CreateAppointment";
-import { Appointments } from "./Appointments/Appointments";
+import { Feed } from "../components/Feed/Feed";
 
 export const MainRouter = () => {
   return (
@@ -17,15 +14,9 @@ export const MainRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/artists" element={<Artists />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/appointments" element={<Appointments />} />
-        <Route
-          path="/createappointment/:artistId"
-          element={<CreateAppointment />}
-        />
       </Routes>
     </>
   );
