105df9689aa860d8847517e7e3e718b0d2b8f586
kulde <ku1de@icloud.com>
2024-04-07 14:34:39 +0200

    Update Home Page view

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,44 @@
+import { CreatePostForm } from "../../components/CreatePostForm/CreatePostForm";
+import { PostsFeed } from "../../components/PostsFeed/PostsFeed";
+
 import "./Home.css";
 
 export const Home = () => {
-  return <div className="home-wrapper"></div>;
+  const refreshPage = () => console.log("refresca o algo");
+
+  return (
+    <div className="home-wrapper">
+      <CreatePostForm onPost={refreshPage} />
+      <PostsFeed />
+      {/*     
+
+// Feed
+
+// Dropdown con los song lists // Get song list
+// Create post with song list and text // Create post with song associated (and user etc)
+
+
+// Listado de posts ordenados por fecha // Get posts with pagination
+// Likes // Create / delete likes with current user
+
+
+
+//////
+
+SONG LISTS
+TEXTAREA
+
+CREATE POST 
+
+/////
+
+
+////
+
+RETRIEVEPOSTS BLABLA
+PAGINATION
+
+//// */}
+    </div>
+  );
 };
